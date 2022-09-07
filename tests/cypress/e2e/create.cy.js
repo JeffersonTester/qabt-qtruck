b3cd0c80460497745e24b44cdd82210904bae0b9
Jefferson Santos <standreschi@gmail.com>
2022-09-07 19:39:29 -0300

    Finalizando o cenário de cadastro de foodtrucks

--- tests/cypress/e2e/create.cy.js
+++ tests/cypress/e2e/create.cy.js
@@ -1,5 +1,6 @@
 
 import mapPage from '../support/pages/Map'
+import createPage from '../support/pages/Create'
 
 describe('Recomendação', () => {
 
@@ -15,15 +16,19 @@ describe('Recomendação', () => {
             latitude: '-23.633123030999226',
             longitude: '-46.658012866973884',
             name: 'Tienda Del Chavo',
-            description: 'O melhor lugar para tomar o suco de limão que parece de groselha e tem sabor de tamarindo.',
-            opening_hours: 'das 14h às 20h'
+            details: 'O melhor lugar para tomar o suco de limão que parece de groselha e tem sabor de tamarindo.',
+            opening_hours: 'das 14h às 20h',
+            open_on_weekends: true
         }
 
         cy.apiCreateUser(user)
         cy.uiLogin(user)
         
         mapPage.createLink()
-        cy.setGeolocation(foodtruck.latitude, foodtruck.longitude)
+        createPage.form(foodtruck)
+        createPage.submit()
+        createPage.modal.haveText('Food truck cadastrado com sucesso!')
+        
 
 
     })
--- tests/cypress/support/pages/Create/index.js
+++ tests/cypress/support/pages/Create/index.js
@@ -1,7 +1,32 @@
 
+import modal from '../components/Modal'
 
 class CreatePage {
 
+    constructor() {
+        this.modal = modal
+    }
+
+    form(foodtruck) {
+        cy.get('input[name=name]').type(foodtruck.name)
+        cy.get('textarea[name=details]').type(foodtruck.details)
+        cy.get('input[name=opening-hours]').type(foodtruck.opening_hours)
+        cy.setGeolocation(foodtruck.latitude, foodtruck.longitude)
+
+        cy.contains('button', foodtruck.open_on_weekends ? 'Sim' : 'Não')
+            .click()
+        // if (foodtruck.open_on_weekends === 'Sim')
+        //     cy.contains('button', 'Sim').click()
+
+        // if (foodtruck.open_on_weekends === 'Não')
+        //     cy.contains('button', 'Não').click()
+    }
+
+    submit() {
+        // Comparando Xpath com o contains do Cypress
+        //button[text()="Cadastrar"]
+        cy.contains('button', 'Cadastrar').click()
+    }
 }
 
 export default new CreatePage()
